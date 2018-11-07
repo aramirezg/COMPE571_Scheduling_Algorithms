8538fa5957b5f93ca7b2fdd3718aa8cebc2804ae
mboado <masahikoboado@gmail.com>
2018-11-07 14:36:36 -0800

    added RM algorithm

--- program.py
+++ program.py
@@ -72,12 +72,80 @@ def RM(*args):
     lcmInts = string2Int(lcmArray)
     print("The LCM is: " + str(LCM(lcmInts)))
 
+    t1Runtime = 0
+    newt1Deadline_Ex = t1Deadline_Ex[1]
 
-##    totalPeriod = 1000
-##    for i in range(1,totalPeriod+1):
-##        print(i)
-##
-##        if(t1Deadline_Ex[1]<)
+    t2Runtime = 0
+    newt2Deadline_Ex = t2Deadline_Ex[1]
+
+    t3Runtime = 0
+    newt3Deadline_Ex = t3Deadline_Ex[1]
+
+    t4Runtime = 0
+    newt4Deadline_Ex = t4Deadline_Ex[1]
+
+    t5Runtime = 0
+    newt5Deadline_Ex = t5Deadline_Ex[1]
+
+##35 times within 300 time units
+
+    totalPeriod = 1000
+    
+    print("newt1Deadline_Ex:", newt1Deadline_Ex)
+    
+    for i in range(1,totalPeriod+1):
+
+        print("i", i)
+        
+        if(t1Runtime < t1Deadline_Ex[2] and i < newt1Deadline_Ex): #if runtime < execution time and i < deadline
+            t1Runtime = t1Runtime + 1
+            print("t1RunTime:", t1Runtime)
+        elif(t2Runtime < t2Deadline_Ex[2] and i < newt2Deadline_Ex): #if runtime < execution time and i < deadline
+            t2Runtime = t2Runtime + 1
+            print("t2RunTime:", t2Runtime)
+        elif(t3Runtime < t3Deadline_Ex[2] and i < newt3Deadline_Ex): #if runtime < execution time and i < deadline
+            t3Runtime = t3Runtime + 1
+            print("t3RunTime:", t3Runtime)
+        elif(t4Runtime < t4Deadline_Ex[2] and i < newt4Deadline_Ex): #if runtime < execution time and i < deadline
+            t4Runtime = t4Runtime + 1
+            print("t4RunTime:", t4Runtime)
+        elif(t5Runtime < t5Deadline_Ex[2] and i < newt5Deadline_Ex): #if runtime < execution time and i < deadline
+            t5Runtime = t5Runtime + 1
+            print("t5RunTime:", t5Runtime)
+        else:
+            print("Idle")
+
+        
+        
+        
+        if(i == newt1Deadline_Ex): #if i == deadline, calculate new deadline and reset runtime
+            newt1Deadline_Ex = newt1Deadline_Ex + t1Deadline_Ex[1]
+            print("newt1Deadline_Ex:", newt1Deadline_Ex)
+            t1Runtime = 0
+
+        if(i == newt2Deadline_Ex): #if i == deadline, calculate new deadline and reset runtime
+            newt2Deadline_Ex = newt2Deadline_Ex + t2Deadline_Ex[1]
+            print("newt2Deadline_Ex:", newt2Deadline_Ex)
+            t2Runtime = 0
+
+        if(i == newt3Deadline_Ex): #if i == deadline, calculate new deadline and reset runtime
+            newt3Deadline_Ex = newt3Deadline_Ex + t3Deadline_Ex[1]
+            print("newt3Deadline_Ex:", newt3Deadline_Ex)
+            t3Runtime = 0
+
+        if(i == newt4Deadline_Ex): #if i == deadline, calculate new deadline and reset runtime
+            newt4Deadline_Ex = newt4Deadline_Ex + t4Deadline_Ex[1]
+            print("newt4Deadline_Ex:", newt4Deadline_Ex)
+            t4Runtime = 0
+
+        if(i == newt5Deadline_Ex): #if i == deadline, calculate new deadline and reset runtime
+            newt5Deadline_Ex = newt5Deadline_Ex + t5Deadline_Ex[1]
+            print("newt5Deadline_Ex:", newt5Deadline_Ex)
+            t5Runtime = 0
+
+    
+    
+            
     
     
 
